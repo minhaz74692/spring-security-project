512f197b9d9eb6697c3912d2d27a49df468ddf00
minhaz74692 <minhaz.ipe.sust@gmail.com>
2025-08-14 02:23:14 +0600

    Added basic secutiry configuration

--- src/main/java/com/mie/secureapp/config/AppSecurityConfig.java
+++ src/main/java/com/mie/secureapp/config/AppSecurityConfig.java
@@ -1,11 +1,27 @@
 package com.mie.secureapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class AppSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws  Exception{
+      return   httpSecurity
+                    .csrf(customizer -> customizer.disable())
+                    .authorizeHttpRequests(request-> request.anyRequest().authenticated())
+                    .formLogin(Customizer.withDefaults())
+                    .httpBasic(Customizer.withDefaults())
+                    .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //Get new session id each time
+                    .build();
+    }
+
+
 //
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
